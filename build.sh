11884117b2149d2e736b409697157fe5ea2c1aa2
Andreabarcia <andreabarcia65@gmail.com>
2023-10-04 12:05:18 -0500

    first commit

--- build.sh
+++ build.sh
@@ -7,3 +7,5 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+DJANGO_SUPERUSER_PASSWORD=somepassword python manage.py createsuperuser --no-input --username=andrea  --email=joe@example.com
